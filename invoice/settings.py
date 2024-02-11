76b679db618cda9dcff134f0d0231f9f2c87a3d7
Irfan <irfan.ahmed@tallykhata.com>
2024-02-11 10:27:22 +0600

    Changed db to pg

--- invoice/settings.py
+++ invoice/settings.py
@@ -82,10 +82,15 @@ WSGI_APPLICATION = 'invoice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
