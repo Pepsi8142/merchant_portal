940c9e28fad2aefc514e6529a7a04e2dae976a28
Irfan <ahmed.1995.irfan@gmail.com>
2024-03-03 21:45:48 +0600

    form consistency

--- main/forms.py
+++ main/forms.py
@@ -10,7 +10,7 @@ class RegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ["username", "email", "shop_name", "password1", "password2"]
+        fields = ["username", "email", "password1", "password2"]
 
 
 class ProductForm(forms.ModelForm):
