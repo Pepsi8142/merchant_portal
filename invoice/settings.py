09bc30c6fe2f4f355bdd67d6ee9ef0555bb04d43
Irfan <ahmed.1995.irfan@gmail.com>
2024-03-10 00:25:48 +0600

    settings

--- invoice/settings.py
+++ invoice/settings.py
@@ -80,23 +80,23 @@ WSGI_APPLICATION = 'invoice.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'merchant_erp',
-#         'USER': 'merchant_erp',
-#         'PASSWORD': '@rae_2k87%_ew',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'merchant_erp',
+        'USER': 'merchant_erp',
+        'PASSWORD': '@rae_2k87%_ew',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
