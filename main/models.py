e0405ab91e79899286526e8d285115dc53c39a25
Irfan <ahmed.1995.irfan@gmail.com>
2024-03-14 22:03:32 +0600

    Product model changed

--- main/models.py
+++ main/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 class Product(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(max_length=250)
     img_url = models.ImageField(upload_to='products/', default=None, null=False)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
