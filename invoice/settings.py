7db786ab6dbaf21f80b6d36ad83169cfdd514cdb
Mahidul <mahidul@tallykhata.com>
2024-03-18 23:48:38 +0600

    6: updated UI and UX

--- invoice/settings.py
+++ invoice/settings.py
@@ -151,3 +151,8 @@ LOGOUT_REDIRECT_URL = '/login'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CORS_ALLOWED_ORIGINS = [
+    "https://erp.mangoitbd.com",
+    # Add more origins if needed
+]
