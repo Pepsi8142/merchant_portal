7683c7833df13ac749ae991f20e52267baccc4cb
Irfan <ahmed.1995.irfan@gmail.com>
2024-03-18 21:52:44 +0600

    Img Field set as non-required

--- main/models.py
+++ main/models.py
@@ -34,14 +34,14 @@ class Supplier(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(max_length=100)
-    img_url = models.ImageField(upload_to='products/', default=None)
+    img_url = models.ImageField(upload_to='products/', null=True)
     buying_price = models.DecimalField(max_digits=10, decimal_places=2, null=False)
     selling_price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     stock_count = models.PositiveIntegerField(validators=[MinValueValidator(1)], null=True)
-    is_cash = models.BooleanField(default=False)
+    is_cash = models.BooleanField(default=False, null=False)
     supplier_id = models.ForeignKey(Supplier, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
